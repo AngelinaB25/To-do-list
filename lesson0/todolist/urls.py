34bd57e6c09f4917c7778d50d0664cc4faeecd46
AngelinaB25 <bestsister.a@gmail.com>
2022-04-26 16:41:31 +0300

    add deals in our list

--- lesson0/todolist/urls.py
+++ lesson0/todolist/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from re import T
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',TemplateView.as_view(template_name = "main.html")),
+    path('monday',TemplateView.as_view(template_name = "monday.html")),
+    path('sunday',TemplateView.as_view(template_name = "sunday.html")),
+    path('tuesday',TemplateView.as_view(template_name = "tuesday.html"))
+
+    
 ]
